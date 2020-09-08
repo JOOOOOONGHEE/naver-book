74c41ceb50064ab52f9a7964e3b37ec755722450
pjh <hstcsys30@naver.com>
2020-09-08 12:51:44 +0900

    Initial commit

---
+++ src/main/java/com/naver/naverbook/NaverbookApplication.java
@@ -0,0 +1,13 @@
+package com.naver.naverbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverbookApplication.class, args);
+	}
+
+}
